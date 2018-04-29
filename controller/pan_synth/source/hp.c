e099dd057c4c2d966977c4b48d3853332a1f447a
Lauri Koponen <lauri.koponen@iki.fi>
2018-04-29 11:06:58 +0200

    Fix highpass filter

--- controller/pan_synth/source/hp.c
+++ controller/pan_synth/source/hp.c
@@ -24,9 +24,9 @@ int32_t hp_update(struct hp_state_t* hp, int32_t value)
 	uint32_t timer_count = timer_value_nonisr();
 	uint32_t timer_delta = timer_count - hp->time;
 
-	if (timer_delta > 0x40000) timer_delta = 1;
+	if (timer_delta > 0x80000) timer_delta = 1;
 	else
-		timer_delta >>= 13;
+		timer_delta >>= 14;
 
 	if (timer_delta == 0) {
 		return hp->value;
@@ -36,24 +36,29 @@ int32_t hp_update(struct hp_state_t* hp, int32_t value)
 	int32_t a = hp->a;
 	int32_t b = hp->b;
 
+	if (value < -32767) value = -32767;
+	else if (value > 32767) value = 32767;
+	value <<= 16;
+
 	int32_t res;
 	while (timer_delta--)
 	{
 		//newvalue = b * value - b * lastvalue + a * lastoutput;
-		res = 0;
 		res = smlal32_hi(0, value, b);
-		res = smlal32_hi(res, hp->value, b);
+		res = smlal32_hi(res, hp->value, -b);
 		res = smlal32_hi(res, hp->last, a);
+		hp->value = value;
+		hp->last = res;
 	}
-	hp->value = value;
-	hp->last = res;
-	return res;
+
+	return res >> 16;
 }
 
 void hp_set_speed(struct hp_state_t* hp, uint16_t speed)
 {
-	int32_t a = (int32_t)((float)0x7FFFFFFF * (1.0f - expf(-100.0f / (float)speed)));
-	int32_t b = (a/2) + 0x40000000;
+	float f = 0.00001f * powf(0.4f / 0.00001f, (float)speed / 65535.f);
+	int32_t a = (int32_t)((float)0x7FFFFFFF * expf(-2.0f * 3.141592654f * f));
+	int32_t b = (a/2) + 0x3FFFFFFF;
 	hp->a = a;
 	hp->b = b;
 }
